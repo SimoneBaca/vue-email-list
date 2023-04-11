dc5420d7c29476bb2631c2273089813487a73901
Simonebaca <simobaco99@gmail.com>
2023-04-11 17:20:33 +0200

    add cicle v.for on the list

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            contacts: {
-                email: 'simo17@gmail.com',
-            }
+            email: []
         }
     },
     methods: {
     },
 
     beforeMount() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-                console.log(response.data);
-                this.contacts.email = response.data.response
-                console.log(response.data.response)
-            });
+        for (let i = 1; i <= 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    this.email.push(response.data.response);
+                    console.log(this.email)
+                });
+        }
     },
-}).mount('#app')
+}).mount('#app')   
+ 
