d59aca987cdccdc74b54b7c25cb033ae11e75467
Simonebaca <simobaco99@gmail.com>
2023-04-11 17:09:33 +0200

    axios in before mount

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+/*Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.*/
+
 const { createApp } = Vue
-
 createApp({
-  data() {
-    return {
-      message: 'Hello!',
-    }
-  }
-}).mount('#app')
+    data() {
+        return {
+            contacts: {
+                email: 'simo17@gmail.com',
+            }
+        }
+    },
+    methods: {
+    },
 
+    beforeMount() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data);
+                this.contacts.email = response.data.response
+                console.log(response.data.response)
+            });
+    },
+}).mount('#app')
